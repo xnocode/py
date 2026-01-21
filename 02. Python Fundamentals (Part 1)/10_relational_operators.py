21487f8c744091a18a97353103573217f196dfc1
Ridoy <xridoynocode@gmail.com>
2026-01-21 11:35:05 +0600

    relational_operators
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ 02. Python Fundamentals (Part 1)/10_relational_operators.py
@@ -0,0 +1,10 @@
+# relational operators in Python
+a = 5 
+b = 15
+
+print("a is equal to b:", a == b)
+print("a is not equal to b:", a != b)
+print("a is less than b:", a < b)
+print("a is greater than b:", a > b)
+print("a is less than or equal to b:", a <= b)
+print("a is greater than or equal to b:", a >= b)
