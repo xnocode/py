9c7d425f2d936f13493809eb39505a5dcb439439
Ridoy <xridoynocode@gmail.com>
2026-01-21 11:35:14 +0600

    assignment
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ 02. Python Fundamentals (Part 1)/11_assignment_operators.py
@@ -0,0 +1,17 @@
+#assignment_operators
+a = 15
+b = 4
+a += b
+print("After a += b, a =", a)
+a -= b
+print("After a -= b, a =", a)
+a *= b
+print("After a *= b, a =", a)
+a /= b
+print("After a /= b, a =", a)
+a //= b
+print("After a //= b, a =", a)
+a %= b
+print("After a %= b, a =", a)
+a **= b
+print("After a **= b, a =", a)
