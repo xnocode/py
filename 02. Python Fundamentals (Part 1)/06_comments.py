ac12cda69634e3bbca99e35aee63c8c757190df6
Ridoy <xridoynocode@gmail.com>
2026-01-21 11:33:18 +0600

    comments
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ 02. Python Fundamentals (Part 1)/06_comments.py
@@ -0,0 +1,9 @@
+# single line comment: this is a comment and will be ignored by python interpreter
+
+"""
+
+This is a multi-line comment.
+It can span multiple lines.
+Useful for longer explanations or documentation.
+
+"""
