d2c6137ad1c19c7a43cbdb27bcb3707a0bcefac3
Ridoy <xridoynocode@gmail.com>
2026-01-26 23:16:16 +0600

    update
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

--- 02. Python Fundamentals (Part 1)/02_next_line.py
+++ 02. Python Fundamentals (Part 1)/02_next_line.py
@@ -1,4 +1,4 @@
 print ("Hello World!")
 print ("Hello Again")
-print ("I like typing this. , this is on a new line")
+print ("I like typing this, this is on a new line")
 print ("This is fun. \nIt's good to learn new things.")
