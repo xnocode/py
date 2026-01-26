c06c2b13c3900e79be184646a892f1d0f80773b3
Ridoy <xridoynocode@gmail.com>
2026-01-26 23:16:40 +0600

    else if
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ 03. Python Fundamentals (Part 2)/03_elif.py
@@ -0,0 +1,9 @@
+color = input("enter color: ")
+if color == "red" :
+    print("stop")
+elif color == "green":
+    print("go")
+elif color == "yellow":
+    print("look")
+else:
+    print("wrong color for traffic lights")
